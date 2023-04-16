d90524dd81090e7efba6a7928529583775dd7724
Lucas <lucas_fernando@ufms.br>
2023-04-16 19:46:10 -0400

    primeiro commit

---
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -0,0 +1,32 @@
+ package com.example.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void jogar(View view){
+
+        TextView textResult;
+
+        textResult = findViewById(R.id.textResult);
+
+        int num = new Random().nextInt(21);
+
+        textResult.setText("Número sorteado " + num);
+
+    }
+
+
+}
